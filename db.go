87351eae5852863acd5776977f5f15b87a70285e
selikakanajmii07 <selikakanajmii@upi.edu>
2026-05-20 23:14:33 +0700

    Update db.go

--- db.go
+++ db.go
@@ -10,7 +10,7 @@ import (
 var DB *sql.DB
 
 func ConnectDB() {
-	connStr := "host=localhost port=5432 user=postgres password=Selikaknjm07 dbname=delivery_db sslmode=disable"
+	connStr := "host=host.docker.internal port=5432 user=postgres password=Selikaknjm07 dbname=delivery_db sslmode=disable"
 
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
