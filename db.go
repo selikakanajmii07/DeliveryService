7920f1db9b4c733d3be5a2f63beaa31311a3856d
selikakanajmii07 <selikakanajmii@upi.edu>
2026-05-21 13:02:48 +0700

    Update db.go

--- db.go
+++ db.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/lib/pq"
 )
@@ -10,7 +12,19 @@ import (
 var DB *sql.DB
 
 func ConnectDB() {
-	connStr := "host=host.docker.internal port=5432 user=postgres password=Selikaknjm07 dbname=delivery_db sslmode=disable"
+	host := os.Getenv("DB_HOST")
+	dbName := os.Getenv("DB_NAME")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+
+	if host == "" || dbName == "" || user == "" || password == "" {
+		log.Fatal("Database environment variables are missing")
+	}
+
+	connStr := fmt.Sprintf(
+		"host=%s port=5432 user=%s password=%s dbname=%s sslmode=disable",
+		host, user, password, dbName,
+	)
 
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
